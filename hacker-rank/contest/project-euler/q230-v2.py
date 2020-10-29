b5c69d86ad84ed149cce971f24cd3b96e0319ea0
Tan Wei Shen <tanweishen97@gmail.com>
2020-10-30 01:53:52 +0800

    Partially solved project euler q230

--- hacker-rank/contest/project-euler/q230-v2.py
+++ hacker-rank/contest/project-euler/q230-v2.py
@@ -1,8 +1,12 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler230/problem
 
+# a = 1415926535
+# b = 8979323846
+# test = 1415926535 8979323846 35
+
 valDict = {}
 
-def solve(a, b, n):  
+def solve(a, b, n):    
   valDict['a'] = a
   valDict['b'] = b
 
@@ -15,42 +19,21 @@ def solve(a, b, n):
   occ = [1, 1]
   occList.append(occ)
   prevOcc = [0, 1]
-  fibIteration = solve_aux(occList, n, occ, prevOcc, len(a), len(b))  
-
+  fibIteration = solve_aux(occList, n, occ, prevOcc, len(a), len(b))    
   # Fib(fibIteration) = Fib(fibIteration - 2) + Fib(fibIteration - 1)
   # n - len(Fib(fibIteration - 2)) <= len(Fib(fibIteratoin -1))
-
   bstTraverse(n - calcLen(occList[fibIteration-2], len(a), len(b)), occList, fibIteration - 1, len(a), len(b))
-"""   count = 0  
-  for i in range (len(accString)):
-    char = accString[i]
-    if char == 'a':
-      val = len(a)
-    else:
-      val = len(b)    
 
-    if count + val >= n:
-      remainder = n - count
-      expandedTarget = valDict[char]
-      target = expandedTarget[remainder-1]
-      print(target)
-      break
-    count += val  
- """
-
-def bstTraverse(n, occList, currIndex, aLen, bLen):
-  
-  if currIndex == 1:
-    print(currIndex, '<< index')
-    myStr = valDict['b']
-    print(myStr[n])
-    return 
-  elif currIndex == 2:
-    print(currIndex, '<< index')
+def bstTraverse(n, occList, currIndex, aLen, bLen):    
+  if currIndex == 0:    
+    
     myStr = valDict['a']
-    print(n)
-    print(myStr)
-    print(myStr[n])
+    print(myStr[n-1])    
+    return 
+  elif currIndex == 1:    
+    
+    myStr = valDict['b']
+    print(myStr[n-1])    
     return 
 
   if n < calcLen(occList[currIndex-2], aLen, bLen):
@@ -59,7 +42,7 @@ def bstTraverse(n, occList, currIndex, aLen, bLen):
     bstTraverse(n - calcLen(occList[currIndex-2], aLen, bLen), occList, currIndex-1, aLen, bLen)
 
 def calcLen(occDict, aLen, bLen):
-  return (occDict[0] * aLen) + (occDict[0] * bLen) 
+  return (occDict[0] * aLen) + (occDict[1] * bLen) 
 
 def solve_aux(occList, n, occDict, prevOcc, aLen, bLen):  
   count = 2
