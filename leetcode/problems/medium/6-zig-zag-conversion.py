ec2dbb9c8576a199c59f886d83b7a8b4940dbaf2
Tan Wei Shen <tanweishen97@gmail.com>
2020-11-02 14:57:02 +0800

    Leet code medium[Optimise] - zig zag conversion from O(n**2) to O(n) time

--- leetcode/problems/medium/6-zig-zag-conversion.py
+++ leetcode/problems/medium/6-zig-zag-conversion.py
@@ -3,33 +3,27 @@
 def solve(s, numRows):
   if numRows == 1:
     return s
-  mat = []
-  for i in range(numRows):
-    mat.append([0] * len(s))
+  mat = [''] * numRows
 
-  pointer = 0
-  rowPointer = 0 
+  pointer = 0  
   back = False
   for i in range (len(s)):
     char = s[i]    
-    mat[pointer][rowPointer] = char
+    mat[pointer] += char
     if pointer == numRows - 1:
       back = True
       
     if pointer == 0:
       back = False
     if back:
-      pointer -= 1
-      rowPointer += 1
+      pointer -= 1      
     else:
       pointer += 1
 
   endStr = ''
   for i in range (len(mat)):    
-    for j in range (len(mat[i])):
-      if mat[i][j] != 0:
-        endStr += mat[i][j]
+    endStr += mat[i]
   return endStr
 
 if __name__ == "__main__":
-  print(solve('AB', 1))
+  print(solve('PAYPALISHIRING', 4))
